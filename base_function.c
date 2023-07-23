14369cb225d3195402bea53c75351308b751e221
Chamour15 <wafae.chamour@gmail.com>
2023-07-23 13:43:59 -0700

    Custom conversion specifiers: b.

--- base_function.c
+++ base_function.c
@@ -37,3 +37,23 @@ char *convert(long int num, int base, int flags, params_t *params)
 	return (ptr);
 }
 
+**
+ * print_binary - function prints unsigned binary number.
+ * @ap: the argument pointer.
+ * @params: the parameters struct definition.
+ *
+ * Return: bytes printed.
+ */
+int print_binary(va_list ap, params_t *params)
+{
+	unsigned int n = va_arg(ap, unsigned int);
+	char *str = convert(n, 2, CONVERT_UNSIGNED, params);
+	int c = 0;
+
+	if (params->hashtag_flag && n)
+		*--str = '0';
+	params->unsign = 1;
+	return (c += print_number(str, params));
+
+} 
+
