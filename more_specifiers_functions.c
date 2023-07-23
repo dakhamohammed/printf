aad5df79b4b1a8c8c6341154f09160f45e3a0972
Chamour15 <wafae.chamour@gmail.com>
2023-07-23 14:46:07 -0700

    Conversion specifier: p.

--- more_specifiers_functions.c
+++ more_specifiers_functions.c
@@ -34,3 +34,22 @@ int print_STRING(va_list ap, params_t *params)
 	return (charCount);
 }
 
+**
+ * print_binary - function prints unsigned binary number.
+ * @ap: the argument pointer.
+ * @params: the parameters struct definition.
+ *
+ * Return: bytes printed.
+ */
+int print_binary(va_list ap, params_t *params)
+{
+	unsigned int n = va_arg(ap, unsigned int);
+	char *str = convert(n, 2, CONVERT_UNSIGNED, params);
+	int c = 0;
+
+	if (params->hashtag_flag && n)
+		*--str = '0';
+	params->unsign = 1;
+	return (c += print_number(str, params));
+
+}
