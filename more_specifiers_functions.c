7e1307180a2d726cba25db131edc7efde02f6b9a
Chamour15 <wafae.chamour@gmail.com>
2023-07-25 12:58:37 -0700

    Custom conversion specifier: %r

--- more_specifiers_functions.c
+++ more_specifiers_functions.c
@@ -55,3 +55,29 @@ int print_address(va_list ap, params_t *params)
 	return (print_number(str, params));
 }
 
+/**
+ * print_rev - prints string in reverse.
+ * @ap: string
+ * @params: the parameters struct definition.
+ *
+ * Return: number bytes printed
+ */
+int print_reverse(va_list ap, params_t *params)
+{
+	int len, sum = 0;
+	char *str = va_arg(ap, char *);
+
+	(void)params;
+
+	if (str)
+	{
+		for (len = 0; *str; str++)
+			len++;
+		str--;
+		for (; len > 0; len--, str--)
+			sum += _putchar(*str);
+	}
+	return (sum);
+}
+
+
